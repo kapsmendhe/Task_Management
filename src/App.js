a98f8e34a543e47bb9cf729347814d0a130ab466
kapil <kapsstudy@gmail.com>
2021-07-10 14:24:17 +0530

    adding new style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import AddTask from "./AddTask";
+import "./style.css";
 
 
 function App() {
   const [state, setState] = useState([]);
   return (
     <div>
+      <h1 style={{ marginLeft: "200px" }}>Task Management</h1>
       <AddTask setState={setState} state={state} />
     </div>
 
